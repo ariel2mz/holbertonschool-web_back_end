64fd9e3fc1cd5e1eab3c7cc77771073bc5801d04
ariel <ariel@gmail.com>
2024-12-08 22:15:43 -0300

    asdasd

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    la funcione s asincronica entonces tipo puede pausarse a la mitad
+    y el resto del programa sigue
+    """
+
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
