3a07df678e79a3bdbfe0273c9fd037b2c08b5e4e
ariel <ariel@gmail.com>
2024-12-08 22:08:06 -0300

    sadas

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -17,14 +17,6 @@ def make_multiplier(multiplier: float) -> Callable[[float], float]:
         Callable[[float], float]:result of multiplying.
     """
     def multiplier_function(value: float) -> float:
-        """
-        asdsadsadsad
-
-        Args:
-            value (float: EL COSO PUM PAM xd ODIO DOCUMENTAR
-        Returns:
-            Float
-        """
         return value * multiplier
 
     return multiplier_function
