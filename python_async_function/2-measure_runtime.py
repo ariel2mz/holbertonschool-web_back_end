6e6f88a2c452a7ed244413dcf29e95d035bbdf39
ariel <ariel@gmail.com>
2024-12-10 13:20:55 -0300

    adasda

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Measure runtime for wait_n.
+"""
+
+import time
+from typing import List
+from concurrent_coroutines import wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """
+    asdsadas
+
+    asdasdasd
+    """
+    start_time = time.perf_counter()
+    await wait_n(n, max_delay)
+    end_time = time.perf_counter()
+    total_time = end_time - start_time
+    return total_time / n
