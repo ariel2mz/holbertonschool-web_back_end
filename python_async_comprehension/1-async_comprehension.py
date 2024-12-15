a2d4e8e181b2bd8156a69dbe02951de79c297e00
ariel <ariel@gmail.com>
2024-12-14 23:43:32 -0300

    sadsadsadsa

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+no tiene sentido comentar esto
+"""
+
+import asyncio
+import random
+from typing import Generator, List
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """
+    jijiji un for de 10
+    y una cosa q saque de google
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
+
+async def async_comprehension() -> List[float]:
+    """
+    asdsadsadsa
+    """
+    return [number async for number in async_generator()]
