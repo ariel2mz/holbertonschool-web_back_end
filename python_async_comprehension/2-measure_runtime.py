dfbc99444b16fbfc76604edd39d275c9fa925590
ariel <ariel@gmail.com>
2024-12-14 23:48:57 -0300

    asdas

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+no tiene sentido comentar esto
+"""
+
+import asyncio
+import random
+from typing import Generator, List
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """
+    jijiji un for de 10
+    y una cosa q saque de google
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
+
+
+async def async_comprehension() -> List[float]:
+    """
+    asdsadsadsa
+    """
+    return [number async for number in async_generator()]
+
+
+async def measure_runtime() -> float:
+    """
+    asdasdsadsada
+    """
+    start_time = asyncio.get_event_loop().time()
+    await asyncio.gather(async_comprehension(), async_comprehension(), async_comprehension(), async_comprehension())
+    total_time = asyncio.get_event_loop().time() - start_time
+    return total_time
