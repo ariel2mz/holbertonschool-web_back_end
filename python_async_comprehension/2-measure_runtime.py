cb7bae2c95ecdf397809da8cc7e3b47af1894522
ariel <ariel@gmail.com>
2024-12-14 23:55:03 -0300

    sadsadsa

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -30,8 +30,7 @@ async def measure_runtime() -> float:
     asdasdsadsada
     """
     start_time = asyncio.get_event_loop().time()
-    await asyncio.gather(
-        async_comprehension(),
+    await asyncio.gather(async_comprehension(),
         async_comprehension(),
         async_comprehension(),
         async_comprehension()
