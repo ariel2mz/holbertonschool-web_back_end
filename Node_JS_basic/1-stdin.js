a2aa929ca6d0b21f529b29c9c38d64951adeef45
ariel <ariel@gmail.com>
2025-01-26 17:21:32 -0300

    sadsadsa

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -9,9 +9,10 @@ const rl = readline.createInterface({
 
 rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
   process.stdout.write('Your name is: ' + name + '\r'); // Ensure carriage return
+  process.stdout.write('\n');
   rl.close();
 });
 
 rl.on('close', () => {
-  process.stdout.write('\nThis important software is now closing');
+  process.stdout.write('This important software is now closing');
 });
