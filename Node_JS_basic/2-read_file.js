0b49f38cc233782839b57c4130e2c9fca4107964
ariel <ariel@gmail.com>
2025-01-26 23:32:25 -0300

    sadsa

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -3,19 +3,19 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const fileContent = fs.readFileSync(path, 'utf8').trim();
-    const lines = fileContent.split('\n').filter((line => line.trim()) !== '');
+    const lines = fileContent.split('\n').filter(line => line.trim() !== '');
     if (lines.length <= 1) throw new Error('Cannot load the database');
-
+    
     const students = lines.slice(1);
     console.log(`Number of students: ${students.length}`);
-
+    
     const fields = {};
-    students.forEach((line => {
+    students.forEach(line => {
       const [firstname, , , field] = line.split(',');
       if (!fields[field]) fields[field] = [];
       fields[field].push(firstname);
-    }));
-
+    });
+    
     Object.entries(fields).forEach(([field, names]) => {
       console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
     });
